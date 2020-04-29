cda622995bca1232400a6fb76175e8c6f95ddcae
Mycha de Vrees <mdvrees@x-com.nl>
2020-04-29 13:30:33 +0200

    set execute permission on docker compose

--- install.sh
+++ install.sh
@@ -67,6 +67,7 @@ if [ ! -f /usr/bin/docker ] && [[ ! -f /usr/local/bin/docker-compose || ! -f /us
     if [ ! -f /usr/local/bin/docker-compose ]; then
       echo "Installing docker-compose"
       curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+      chmod +x /usr/local/bin/docker-compose
     fi
   else
     echo "You can download both at:"
