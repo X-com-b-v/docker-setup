a5b89e3ca05c7abf5fadb0ef07962c07c3244abf
Mycha de Vrees <m.devrees@gmail.com>
2022-10-18 20:24:07 +0200

    updates to phprun and xdebug

--- dep/phprun.sh
+++ dep/phprun.sh
@@ -24,48 +24,28 @@ if [ ! -f "/home/web/.git-completion.bash" ]; then
   bash /home/web/git-autocomplete.sh
 fi
 
-if [ ! -d "/home/web/bin" ]; then
-  mkdir -p /home/web/bin
+BIN_DIR="/home/web/bin"
+if [ ! -d "$BIN_DIR" ]; then
+  mkdir -p $BIN_DIR
 fi
+
+declare -A array
+array[composer]=https://getcomposer.org/composer.phar
+array[dep]=http://deployer.org/deployer.phar
+array[magerun]=https://files.magerun.net/n98-magerun.phar
+array[magerun2]=https://files.magerun.net/n98-magerun2.phar
+array[symfony]=https://symfony.com/installer
+for i in "${!array[@]}"
+do
+    if [ ! -f "$BIN_DIR/$i" ]; then
+        curl -LsS ${array[$i]} -o $BIN_DIR/$i
+        chmod +x $BIN_DIR/$i
+    fi
+done
 
 if [ ! -f "/home/web/bin/starship" ]; then
     sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- --bin-dir /home/web/bin --force
     echo 'eval "$(starship init bash)"' >> /home/web/.bashrc
-fi
-
-if [ ! -f "/home/web/bin/composer" ]; then
-  curl https://getcomposer.org/composer.phar -o /home/web/bin/composer
-  chmod +x /home/web/bin/composer
-fi
-
-if [ ! -f "/home/web/bin/dep" ]; then
-  curl http://deployer.org/deployer.phar -o /home/web/bin/dep
-  chmod +x /home/web/bin/dep
-fi
-
-if [ ! -f "/home/web/bin/magerun" ]; then
-  curl https://files.magerun.net/n98-magerun.phar -o /home/web/bin/magerun
-  chmod +x /home/web/bin/magerun
-fi
-
-if [ ! -f "/home/web/bin/magerun2" ]; then
-  curl https://files.magerun.net/n98-magerun2.phar -o /home/web/bin/magerun2
-  chmod +x /home/web/bin/magerun2
-fi
-
-if [ ! -f "/home/web/bin/pestle" ]; then
-  curl http://pestle.pulsestorm.net/pestle.phar -o /home/web/bin/pestle
-  chmod +x /home/web/bin/pestle
-fi
-
-if [ ! -f "/home/web/bin/symfony" ]; then
-  curl -LsS https://symfony.com/installer -o /home/web/bin/symfony
-  chmod a+x /home/web/bin/symfony
-fi
-
-if [ ! -f "/home/web/bin/deb" ]; then
-  curl -LsS http://deployer.org/deployer.phar -o /home/web/bin/dep
-  chmod a+x /home/web/bin/dep
 fi
 
 if [ ! -d "/home/web/.nvm" ]; then
