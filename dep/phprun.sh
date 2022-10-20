3dd69a2b354df80a4e46d74766b559f863da367b
Mycha de Vrees <m.devrees@gmail.com>
2022-10-20 20:41:21 +0200

    nvm new version

--- dep/phprun.sh
+++ dep/phprun.sh
@@ -49,7 +49,7 @@ if [ ! -f "/home/web/bin/starship" ]; then
 fi
 
 if [ ! -d "/home/web/.nvm" ]; then
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 fi
 
 sudo php-fpm -R
