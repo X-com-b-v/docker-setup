905ae7d0d2b5d7437819e80b673165cc115a5c1e
Mike Rooijackers <mrooijackers@x-com.nl>
2024-11-22 10:16:00 +0100

    DEVENV-1 Documentatie bijwerken en verplaatsen

--- docker/apache/run.sh
+++ docker/apache/run.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-apachectl -t
+echo "Validating Apache configuration..."
+if apachectl -t; then
+    echo "Configuration is valid."
+else
+    echo "Apache configuration is invalid!"
+    exit 1
+fi
 
+echo "Restarting Apache service..."
 if service apache2 restart; then
+    echo "Apache restarted successfully. Following error log:"
     tail -f /var/log/apache2/error.log
 else
+    echo "Failed to restart Apache! Showing last 100 error log entries:"
     tail -n 100 /var/log/apache2/error.log
     exit $?
 fi
